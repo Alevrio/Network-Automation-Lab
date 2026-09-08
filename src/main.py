19187174cb380498a1e7e628f9a0ca54b7b14bcd
Alevrio <princepanlilio4@gmail.com>
2026-09-08 08:36:13 +0800

    feat: included deserialization

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("Network Automation Lab")
+import json
+
+with open("data/devices.json") as file:
+    devices = json.load(file)
+
+for hostname, device in devices.items():
+    
+    if device["online"]:
+        status = "ONLINE"
+    else: 
+        status = "OFFLINE"
+    print(f"{hostname} - {device["ip"]} - {status}")
+    
